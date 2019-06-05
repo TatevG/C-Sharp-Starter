ab8b7bb7d97c93d88f8f91723cb59e87987dd46a
TatevikV <tatevikfvardanyan@gmail.com>
2019-06-05 11:41:41 +0400

    Methods

---
+++ Methods/016_Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void DrawRectangle(int width, int height)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            int sum = Add(2, 5);
+            Console.WriteLine(sum);
+            DrawRectangle(8, 3);
+            DrawRectangle(7, 7);
+            DrawRectangle(4, 3);
+        }
+    }
+}
--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+// Методы (Процедуры).
+
 namespace Methods
 {
     class Program
     {
-        static void Main(string[] args)
+        // На 12-й строке, создаем метод с именем Procedure, который ничего не принимает и ничего не возвращает.
+        // В теле метода, на 14-й строке выводим на экран строку - Hello!        
+
+        static void Procedure()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+        }
+
+        static void Main()
+        {
+            // В теле метода Main на 21-й строке, вызываем метод Procedure.
+
+            Procedure();
+
+            // Delay.
+            Console.ReadKey();
         }
     }
 }
